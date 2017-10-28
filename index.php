a3a862a8f1b7fe8c05051394a36900e2f0536349
vaggelis <vako88@gmail.com>
2017-10-28 22:24:36 +0300

    International

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
  * Date: 10/14/2017
  * Time: 9:41 PM
  */
-require_once 'config.php';
-use Classes\Various\International;
+//require_once 'config.php';
+include "Classes/various/International.php";
 
 // Εδω Τασούλα μπορείς να τεσταρεις αυτα που κάνεις
-
+use Classes\Various\International;
 print_r(International::get_values_for_key(5));
