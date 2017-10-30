e2f9d74b5f2da4dac4754cb3a33b8a599cfdcab3
Vangelis Kostopoulos <vaggos_kostopoulos@yahoo.gr>
2017-10-30 20:46:53 +0200

    Add book.html view and use ng-include to profile.html and searchBooks.html to included when it should.

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -35,17 +35,3 @@ mscApp.config(['$routeProvider',
             redirectTo: '/#login'
         });
     }]);
-
-mscApp.directive('myEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.myEnter);
-                });
-
-                event.preventDefault();
-            }
-        });
-    };
-});
